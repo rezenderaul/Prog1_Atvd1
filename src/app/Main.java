fc0b888f15875d34fd9234f48b0d9ce9ddc9a98a
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2020-09-09 10:17:06 -0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+import entity.Calculadora;
+
+/**
+ *
+ * @author raulrezende
+ */
+public class Main {
+    public static void main (String Args[]) {
+        
+        Calculadora calc = new Calculadora();
+        
+        System.out.println(calc.somar(5, 10));
+
+        System.out.println(calc.subtrair(8, 4));
+
+        System.out.println(calc.multiplicar(2, 2));
+
+        System.out.println(calc.dividir(5, 5));
+        
+    }
+}
---
+++ src/entity/Calculadora.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author raulrezende
+ */
+public class Calculadora {
+
+    public double somar (double x, double y)
+    {
+        return x + y;
+    }
+    
+    public double subtrair (double x, double y)
+    {
+        return x - y;
+    }
+    
+    public double multiplicar (double x, double y)
+    {
+        return x * y;
+    }
+    
+    public double dividir (double x, double y)
+    {
+        return x / y;
+    }
+    
+    
+}
